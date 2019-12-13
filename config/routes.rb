2e5d5f92618346a353a648be2d9111d88bf66278
pauloprescendo <paulovinicius1994@hotmail.com>
2019-12-13 15:33:36 -0300

    last day semana super fullstack

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,17 @@ Rails.application.routes.draw do
   # get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "chats#index"
+
+  get "/manifest.json", to: "service_workers#manifest"
+  get "/service-worker.js" => "service_workers#service_worker"
+
+  resources :contacts, only: %i(index new create destroy)
 
   resources :contacts, only: [:index, :new, :create, :destroy]
+  resources :chats, only: [:index, :show]
+
+  resources :chats, only: [:index, :show] do
+    resources :messages, only: :create, defaults: { format: :js }
+  end
 end
